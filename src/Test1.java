c43cc4b21ca9ab355ed8903f15ec43dfca0d0dd4
OlhaS28 <79612602+OlhaS28@users.noreply.github.com>
2022-10-17 20:11:33 -0500

    Olga test

--- src/Test1.java
+++ src/Test1.java
@@ -2,5 +2,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Test2");
         System.out.println("Test3");
+        System.out.println("Test4");
     }
 }
